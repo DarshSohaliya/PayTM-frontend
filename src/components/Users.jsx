318487e45efd781abcea37c1d4d375ad69cfad4e
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2024-03-04 15:59:18 +0530

    Added logic for filter and add componens User and Balance

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { Button } from "./Button"
+import { useNavigate } from "react-router-dom"
 
 export const Users = () => {
 
@@ -25,9 +27,38 @@ export const Users = () => {
                }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border-rounded border-slate-200" />
           </div>
           <div >
-
+                {users.map(user => <User user={user} /> )}
           </div>
         </>
     )
 
+}
+
+
+function User({user}) {
+
+    const navigate = useNavigate()
+    return (
+        <>
+            <div className="flex justify-between">
+                <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                    <div className="flex flex-col justify-center h-full text-xl">
+                        {user.firstName[0]}
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center h-full">
+                    <div>
+                        {user.firstName} {user.lastName}
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="flex flex-col justify-center h-full">
+                <Button onClick={(e) => {
+                    navigate('/send?id' + user._id + "&name=" + user.firstName)
+                }} label={"Send Money"}/>
+            </div>
+        </>
+    )
 }
